b9cb74b6328a314ea04d635b61a29522076e4fc0
Sam Donohoo <sad0046@auburn.edu>
2025-02-06 14:38:54 -0600

    Loading in Tips.csv file and Reading in CSV.

--- Donohoo_Samantha_CodingPractice_1.R
+++ Donohoo_Samantha_CodingPractice_1.R
@@ -23,23 +23,27 @@ zdf <- data.frame(z, zlog)
 
 colnames(zdf) <- c("zvec", "zlogic")
 
-# Make a new column in your dataframe equal to zvec squared (i.e., z2). Call the new column zsquared. 
+# Make a new column in your dataframe equal to zvec squared (i.e., z2). Call the new column zsquare. 
 
-zdf$zsquared <- sqrt(zdf$zvec)
+zdf$zsquare <- (zdf$zvec)^2
 
 # Subset the dataframe with and without the subset() function to 
-# only include values of zsquared greater than 10 and less than 100
+# only include values of zsquare greater than 10 and less than 100
 
-zdf_nosubsetcommand <- zdf[zdf$zsquared > 10 & zdf$zsquared < 100,]
+zdf_nosubsetcommand <- zdf[zdf$zsquare > 10 & zdf$zsquare < 100,]
 
-zdf_subset <- subset(zdf, (zdf$zsquared > 10) & (zdf$zsquared < 100))
+zdf_subset <- subset(zdf, (zdf$zsquare > 10) & (zdf$zsquare < 100))
 
 # Subset the zdf dataframe to only include the values on row 26
 
 zdf_Row26 <- zdf[26,]
 
-# Subset the zdf dataframe to only include the values in the column zsquared in the 180th row.
+# Subset the zdf dataframe to only include the values in the column zsquare in the 180th row.
 
 zdf_Row180_zsquare <- zdf[180,3]
 
 # Annotate your code, commit the changes and push it to your GitHub
+
+# Read in the data into R so that the missing values are properly coded.
+
+Tips_Data <- read.csv("TipsR.csv", header = TRUE, sep = ",", na.strings = ".")
